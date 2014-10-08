a326ea9bf262a9f97fc7cd55d77f591c515c7640
homeski <homeski@cox.net>
2014-10-07 18:42:42 -0700

    Bookmark now woots when it is clicked

--- plug.dj.aux.js
+++ plug.dj.aux.js
@@ -1,8 +1,13 @@
 javascript:(function(){
+	// Woot when the bookmark is clicked
+	$('woot').click();
+
+	// Woot on song change
 	API.on(API.ADVANCE, function(e) { 
 		$('#woot').click(); 
 	});
 
+	// Enable spacebar mute
 	$(document).keypress(function(e) {
 		if (e.which == 32 && $(':focus').is('input') == false) {
 			$('#volume > .button').click();
