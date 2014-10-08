79d0c9956cb24bd477c52aa6d9b0891fca7bd9a6
homeski <homeski@cox.net>
2014-10-07 23:19:58 -0700

    Commenting

--- plug.dj.aux.js
+++ plug.dj.aux.js
@@ -9,14 +9,16 @@ javascript:(function(){
 
 	// Key listeners
 	$(document).keypress(function(e) {
+		// Check if we're typing
 		if ($(':focus').is('input') == true)
 			return;
 
 		switch (e.which) {
-			case 32: // spacebar
+			case 32: // Spacebar
 				$('#volume > .button').click();
 				break;
 			case 103: // g
+				// If song isn't already grabbed
 				if ($('#grab.selected').length < 1) {
 					$('#grab').click();
 					$('.grab > .menu > ul > li > .icon-active-active').parent().mousedown();
